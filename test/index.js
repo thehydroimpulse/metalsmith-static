7696e766fda912cda5a8b6677cea6ed30627bd06
Mayo Jordanov <mayo@oyam.ca>
2014-08-29 01:36:33 -0700

    Recursive copy
    When specifying a file, or changing directory layout, recursively create the destination directories, so the target can be copied.
    
    Instroduces new flag: createDest (defaults to false to preserve original behaviour).

--- test/index.js
+++ test/index.js
@@ -13,3 +13,20 @@ describe('test', function() {
       });
   });
 });
+
+describe('test', function() {
+  it('should create destination directory recursively', function(done) {
+    var metalsmith = Metalsmith('test/fixtures/recurse');
+    metalsmith
+      .use(asset({
+        "src": "assets/media/css/styles.css",
+        "dest": "css/style.css",
+        "createDest": true
+      }))
+      .build(function(err) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
